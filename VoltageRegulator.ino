ad5c0dbeb0b4d6afe92e9673e8df102d17e9c033
Md Mohitul Haque <91718614+Mohitul-Haque@users.noreply.github.com>
2023-02-08 15:00:59 -0400

    Add files via upload

---
+++ VoltageRegulator.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+int OFFSET = 70;
+
+int val;
+
+
+#define LED1 9
+
+void setup(){   
+    pinMode(LED1,OUTPUT);
+    Serial.begin(9600);
+    Serial.println("Voltage: ");   
+    
+}
+
+void loop(){       
+    float voltage;       
+    val = analogRead(A0);
+    voltage = map(val,0,1023,0,2500) - OFFSET; //mV
+//    temp=val_1/4.092;
+//    val_1=(int)temp;  
+//    val_2=((val_1%100)/10);
+    Serial.print(voltage/100);
+    Serial.println(" V");
+    delay(1000); 
+}
