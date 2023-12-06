db0821ac717c0720e788877340148a287743157f
Md Mohitul Haque <91718614+Mohitul-Haque@users.noreply.github.com>
2023-12-06 06:42:37 -0400

    Update api.cpp

--- api.cpp
+++ api.cpp
@@ -5,7 +5,6 @@
 #include <math.h>
 
 // NOTE!!!
-// Don't assume the below API follows good coding or JCA conventions.
 // Follow your own best judgement for your own code.
 // Feel free to rework this API if you like (not required).
 
